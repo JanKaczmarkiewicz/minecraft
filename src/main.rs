1e1d737ad8623e24dba9b4986ec9ab3bcc211a21
jkaczmarkiewcz <positiveKiddo@gmail.com>
2026-08-27 22:45:01 +0200

    feat: add bottom surface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use glam::Vec3;
+use glam::vec3;
 use lib::{buffer::Buffer, objects::sphere::Sphere, tracerays::trace_rays};
 
 use minifb::{Key, Window, WindowOptions};
@@ -20,21 +20,23 @@ fn main() {
         panic!("{}", e);
     });
 
-    let camera = Vec3 {
-        x: 0.0,
-        y: 0.0,
-        z: -1.0,
-    };
+    let camera = vec3(0.0, 0.0, -1.0);
 
     window.set_target_fps(3);
     buffer.fill_all(0);
     trace_rays(
         &mut buffer,
         camera,
-        &vec![Box::new(Sphere {
-            center: camera,
-            radius: 0.5,
-        })],
+        &vec![
+            Box::new(Sphere {
+                center: camera,
+                radius: 0.5,
+            }),
+            Box::new(Sphere {
+                center: camera + vec3(0.0, -100.5, 0.0),
+                radius: 100.0,
+            }),
+        ],
     );
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
