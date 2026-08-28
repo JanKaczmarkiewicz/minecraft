7ef14d0b5bf60d4a567028563d6885b071a30ff3
jkaczmarkiewcz <positiveKiddo@gmail.com>
2026-08-28 14:53:50 +0200

    feat: antialiasing

--- src/lib/color.rs
+++ src/lib/color.rs
@@ -1,14 +1,8 @@
 use glam::Vec3;
 
 pub fn pixel_from_rgb(v: Vec3) -> u32 {
-    let r = 0.0..=1.0;
+    let v = v.min(Vec3::ONE).max(Vec3::ZERO) * u8::MAX as f32;
 
-    assert!(r.contains(&v.x));
-    assert!(r.contains(&v.y));
-    assert!(r.contains(&v.z));
-
-    let v = v * u8::MAX as f32;
-
-    let (r, g, b) = (v.x as u8 as u32, v.y as u8 as u32, v.z as u8 as u32);
+    let (r, g, b) = (v.x as u32, v.y as u32, v.z as u32);
     (r << 16) | (g << 8) | b
 }
--- src/lib/tracerays.rs
+++ src/lib/tracerays.rs
@@ -5,6 +5,7 @@ use crate::{
     ray::Ray,
 };
 use glam::{Vec3, vec3};
+use rand::RngExt;
 
 fn perpendicular_plane(a: Vec3) -> (Vec3, Vec3) {
     let up = (Vec3::Y - Vec3::Y.project_onto_normalized(a)).normalize();
@@ -15,31 +16,47 @@ fn perpendicular_plane(a: Vec3) -> (Vec3, Vec3) {
 }
 
 pub fn trace_rays(buffer: &mut Buffer, camera: Vec3, world: &HittableList) {
+    const SAMPLES_PER_PIXEL: usize = 40;
+
     let viewport_height = 2.0;
     let pixel_size = viewport_height / buffer.height as f32;
 
     let (vertical, horizontal) = perpendicular_plane(camera);
 
+    let mut rng = rand::rng();
+
     for i in 0..buffer.width {
         for j in 0..buffer.height {
-            let x = i as f32 + 0.5 - buffer.width as f32 / 2.0;
-            let y = -(j as f32 + 0.5 - buffer.height as f32 / 2.0);
+            let color = (0..SAMPLES_PER_PIXEL)
+                .map(|_| {
+                    // This works because rng.random::<f32>() gives value from uniform distribution (0.0..1.0)
+                    // then rays will include all pixel are not only center as previously
+                    let x = i as f32 + rng.random::<f32>() - buffer.width as f32 / 2.0;
+                    let y = j as f32 + rng.random::<f32>() - buffer.height as f32 / 2.0;
 
-            let ray = Ray {
-                direction: vertical * pixel_size * y + horizontal * pixel_size * x + camera, //  todo scale to unit? or unif
-                origin: vec3(0.0, 0.0, 0.0),
-            };
+                    let direction =
+                        -y * vertical * pixel_size + x * horizontal * pixel_size + camera;
 
-            let color = if let Some((object, t)) = first_hit(world, &ray) {
-                // object hit normal
-                (object.normal(ray.at(t)) + Vec3::ONE) * 0.5
-            } else {
-                // space
-                let a = 0.5 * (ray.direction.normalize().y + 1.0);
-                let white = vec3(1.0, 1.0, 1.0);
-                let blue = vec3(0.5, 0.7, 1.0);
-                (1.0 - a) * white + a * blue
-            };
+                    let ray = Ray {
+                        direction,
+                        origin: vec3(0.0, 0.0, 0.0),
+                    };
+
+                    if let Some((object, t)) = first_hit(world, &ray) {
+                        // object hit normal
+                        (object.normal(ray.at(t)) + Vec3::ONE) * 0.5
+                    } else {
+                        // space
+                        let a = 0.5 * (ray.direction.normalize().y + 1.0);
+                        let white = vec3(1.0, 1.0, 1.0);
+                        let blue = vec3(0.5, 0.7, 1.0);
+                        (1.0 - a) * white + a * blue
+                    }
+                })
+                .sum::<Vec3>()
+                / SAMPLES_PER_PIXEL as f32;
+
+            // println!("{:?}", color);
 
             buffer.fill_pixel((i, j), pixel_from_rgb(color));
         }
